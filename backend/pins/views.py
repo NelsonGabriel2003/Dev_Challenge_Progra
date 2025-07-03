ef54e58fd6ed60698980bcf92749a8d6a4da8b32
SaidNa14-02 <msnavarretem@puce.edu.ec>
2025-07-03 15:49:33 -0500

    Modified views to bring all the routes from the database

--- backend/pins/views.py
+++ backend/pins/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Ruta
 
 # Create your views here.
 
 def index(request):
+    rutas = Ruta.objects.all()
+    context = {'rutas': rutas}
     return render(request, 'index.html')
+
+
