3e1800da47f4d1999dca95f761177983daeb4a86
Said Navarrete <msnavarretem@puce.edu.ec>
2025-07-04 10:34:35 -0500

    Added url to edit routes

--- backend/pins/urls.py
+++ backend/pins/urls.py
@@ -6,4 +6,5 @@ app_name = 'pins'
 urlpatterns = [
     path('', views.index, name='index'),
     path('agregar-ruta/', views.agregar_ruta, name='agregar_ruta'),
+    path('editar-ruta/<int:ruta_id>/', views.editar_ruta, name='editar_ruta'),
 ]
